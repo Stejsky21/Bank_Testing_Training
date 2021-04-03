b8ac2e47e6f1b65133197ca758212dd0d92b9d23
Tomáš Stejskal <Stejsky211@seznam.cz>
2021-04-03 16:44:51 +0200

    Added new test methods

--- BankTests/BankAccountTests.cs
+++ BankTests/BankAccountTests.cs
@@ -7,11 +7,6 @@ namespace BankTests
     public class BankAccountTests
     {
         [TestMethod]
-        public void TestMethod1()
-        {
-
-        }
-        [TestMethod]
         public void Debit_WithValidAmount_UpdatesBalance()
         {
             // Arrange
@@ -26,6 +21,49 @@ namespace BankTests
             // Assert
             double actual = account.Balance;
             Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+
+        [TestMethod]
+        public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            // Arrange
+            double beginningBalance = 15;
+            double debitAmount = -100;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // Act and assert
+            Assert.ThrowsException<System.ArgumentOutOfRangeException>(() => account.Debit(debitAmount));
+        }
+
+        [TestMethod]
+        public void Debit_WhenAmountIsMoreThanBalance_ShouldThrowArgumentOutOfRange()
+        {
+            // Arrange
+            //double beginningBalance = 15;
+            //double debitAmount = 100;
+            //BankAccount account = new BankAccount("Mr. Tomas", beginningBalance);
+
+            // Act and assert
+            //Assert.ThrowsException<System.ArgumentOutOfRangeException>(() => account.Debit(debitAmount));
+
+            // Arrange
+            double beginningBalance = 15;
+            double debitAmount = 100;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // Act
+            try
+            {
+                account.Debit(debitAmount);
+            }
+            catch (System.ArgumentOutOfRangeException e)
+            {
+                // Assert
+                StringAssert.Contains(e.Message, BankAccount.DebitAmountExceedsBalanceMessage);
+                return;
+            }
+
+            Assert.Fail("The expected exception was not thrown.");
         }
     }
 }
